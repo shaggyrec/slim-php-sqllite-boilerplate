bea0df554fcc0f6412ceaddd6d8e40041e6373c6
Alexander Shogenov <shaggyrec@users.noreply.github.com>
2020-08-04 09:22:59 +0300

    Remove unused code

--- src/functions/http.php
+++ src/functions/http.php
@@ -20,14 +20,3 @@ function errorResponse(int $code, string $message): ResponseInterface
         ->withBody($body)
         ->withHeader('Content-Type', 'application/json;charset=utf-8');
 }
-
-/**
- * @param string $span
- * @throws Controllers\Exception\BadRequest
- */
-function assertGoodSpanParameter(string $span)
-{
-    if (!in_array($span, ['day', 'month', 'year'])) {
-        throw new Controllers\Exception\BadRequest("Parameter 'span' should be one of 'day', 'month', 'year'");
-    }
-}
